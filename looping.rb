80b9f9139a81e4873e3456d049610248b7cc25e0
Doris Morales <moralesdoris880@gmail.com>
2022-02-02 14:08:30 -0500

    commit tree 4c3dff9051e135d9b0e2b75bab65320a0c5b32aa

--- looping.rb
+++ looping.rb
@@ -1,6 +1,11 @@
 def happy_new_year
-  # your code here
+  i=11
+  while i > 1
+    puts i -=1
+  end
+  puts "Happy New Year!"
 end
+
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
@@ -16,9 +21,32 @@ def fizzbuzz(num)
 end
 
 def fizzbuzz_printer
-  # your code here
+  i=0
+  until i==100
+    i+=1
+    if i % 3 == 0 && i % 5 == 0
+      puts "FizzBuzz"
+    elsif i % 3 == 0
+      puts"Fizz"
+    elsif i % 5 == 0
+      puts"Buzz"
+    else
+      puts i
+    end
+    
+  end
 end
 
+
+#Don't use the built-in .reverse method. Instead, loop through the characters in the input string and reverse it.
+str="Doris"
 def reverse_string(str)
-  # your code here
+  reverseStr= ""
+  i=0
+  (str.length).times do |i|
+    reverseStr= str[i]+reverseStr
+  end
+   reverseStr
 end
+
+
